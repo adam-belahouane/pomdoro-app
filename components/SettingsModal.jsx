33bad91890523adf38be8129141436d7b0f062da
marco belahouane <nfk18@hotmail.co.uk>
2023-05-09 09:04:16 +0100

    adam adjusted my code

--- components/SettingsModal.jsx
+++ components/SettingsModal.jsx
@@ -3,37 +3,44 @@
 const SettingModal = ({isOpen, setIsOpen}) => {
 
     return (
-        <div className="fixed z-10 inset-0 overflow-y-auto">
-          <div className="flex items-center justify-center min-h-screen">
+        <div className="fixed z-10 inset-0 overflow-y-auto  ">
+          
+          <div className="flex items-center justify-center min-h-screen ">
             <div
               className="fixed inset-0 transition-opacity"
               aria-hidden="true"
             >
               <div className="absolute inset-0 bg-black opacity-75"></div>
             </div>
-            <div className="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
-              <div className="bg-gray-100 px-4 py-3 border-b ">
+            <div className="bg-white rounded-lg shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+              <div className="bg-gray-100 px-4 py-3 border-b flex justify-between ">
                 <h3 className="text-3xl font-bold">
                   Settings
                 </h3>
-                <div onClick={() => setIsOpen(false)}>
+                <div onClick={() => setIsOpen(false)} className="cursor-pointer ">
                     x
                 </div>
               </div>
+
               <div className="p-4">
-                {/* Modal content goes here */}
-                <p className="text-gray-700">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
+                <h1 className="text-xl font-bold items-center">Time (Minutes)</h1>
+                <div className="flex flex-1 w-full">
+                  <div className=" w-1/3 mr-3">
+                    <h3>pomodoro</h3>
+                    <div className="bg-[#EFF1FA]">25</div>
+                  </div>
+                  <div className="w-1/3 mr-3">
+                    <h3>short break</h3>
+                    <div className="bg-[#EFF1FA]">5</div>
+                  </div>
+                  <div className="w-1/3">
+                    <h3>long break</h3>
+                    <div className="bg-[#EFF1FA]">10</div>
+                  </div>
+                </div>
               </div>
               <div className="bg-gray-100 px-4 py-3 border-t flex justify-end">
-                <button
-                  className="text-gray-500 font-medium"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Close
-                </button>
+                
               </div>
             </div>
           </div>
